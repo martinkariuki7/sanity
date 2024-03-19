2e3df6b6e82cbc70f9b935fe669ac500b0559125
Martin Kariuki <martinkariuki7@gmail.com>
2024-03-19 15:08:43 +0100

    Pull dummy data

--- app/_components/Posts.tsx
+++ app/_components/Posts.tsx
@@ -6,12 +6,12 @@ export default function Posts({ person }: { person: SanityDocument[] }) {
     <main className="container mx-auto grid grid-cols-1 divide-y divide-blue-100">
       {person?.length > 0 ? (
         person.map((post) => (
-          <Link key={post._id} href={post.slug.current}>
-            <h2 className="p-4 hover:bg-blue-50">{post.title}</h2>
+          <Link key={post._id} href="#">
+            <h2 className="p-4 hover:bg-blue-50">{post.fullName}</h2>
           </Link>
         ))
       ) : (
-        <div className="p-4 text-red-500">No posts were found</div>
+        <div className="p-4 text-red-500">No profiles found</div>
       )}
     </main>
   );
--- sanity/lib/queries.ts
+++ sanity/lib/queries.ts
@@ -1,5 +1,5 @@
 import { groq } from "next-sanity";
 
-export const POSTS_QUERY = groq`*[_type == "person" && defined(slug)]`;
+export const POSTS_QUERY = groq`*[_type == "person"]`;
 
 export const POST_QUERY = groq`*[_type == "person" && slug.current == $slug][0]`;
