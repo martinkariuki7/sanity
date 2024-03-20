4106bbd0de698c61b29f955cf65d353d4da04317
Martin Kariuki <martinkariuki7@gmail.com>
2024-03-20 11:05:52 +0100

    Add profile image

--- app/_components/Posts.tsx
+++ app/_components/Posts.tsx
@@ -1,5 +1,6 @@
 import { SanityDocument } from "next-sanity";
 import Link from "next/link";
+import Image from "next/image";
 
 export default function Posts({ person }: { person: SanityDocument[] }) {
   return (
@@ -8,6 +9,13 @@ export default function Posts({ person }: { person: SanityDocument[] }) {
         person.map((post) => (
           <Link key={post._id} href="#">
             <h2 className="p-4 hover:bg-blue-50">{post.fullName}</h2>
+            <Image
+              src="https://cdn.sanity.io/images/b4zp8ctq/production/465e63f9113dec3405a38ca000cf432e3ca00383-960x960.webp"
+              width={300}
+              height={300}
+              alt="Picture of the author"
+              priority={true}
+            ></Image>
           </Link>
         ))
       ) : (
